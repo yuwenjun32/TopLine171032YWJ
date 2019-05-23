11dfdf5ee035bf53b760d63ef1c8208aafbe7a37
yuwenjun32 <1398981815@qq.com>
2019-05-23 15:41:16 +0800

    我的模块

--- app/src/main/java/cn/edu/gdpt/topline171032ywj/Fragment/MeFragment.java
+++ app/src/main/java/cn/edu/gdpt/topline171032ywj/Fragment/MeFragment.java
@@ -1,6 +1,7 @@
 package cn.edu.gdpt.topline171032ywj.Fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,13 +9,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import cn.edu.gdpt.topline171032ywj.R;
+import cn.edu.gdpt.topline171032ywj.activity.LoginActivity;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MeFragment extends Fragment {
-
-
+public class MeFragment extends Fragment implements View.OnClickListener {
+    private View view;
+    CircleImageView iv_avatar;
     public MeFragment() {
         // Required empty public constructor
     }
@@ -24,7 +27,26 @@ public class MeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_me, container, false);
+        view=inflater.inflate(R.layout.fragment_me, container, false);
+        initView(view);
+        return view;
     }
 
+    private void initView(View view) {
+        iv_avatar=(CircleImageView)view.findViewById(R.id.iv_avatar);
+        iv_avatar.setImageResource(R.drawable.default_head);
+        iv_avatar.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.iv_avatar:
+                //跳转到登录界面
+                Intent intent=new Intent(getContext(), LoginActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
 }
---
+++ app/src/main/java/cn/edu/gdpt/topline171032ywj/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.gdpt.topline171032ywj.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.edu.gdpt.topline171032ywj.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
